095120cb5d552cc12e455a61397bd1fd80ba7efb
UIUX Lab <uiuxlab@gmail.com>
2023-04-11 22:26:27 +0800

    Add language generated scripts

---
+++ scripts/i18n/generate-translation-file.js
@@ -0,0 +1,58 @@
+/**
+ *  Generate a translation file
+ * 
+ */
+const path = require('path');
+const fs = require('fs');
+const componentPath = path.resolve(__dirname, '../../src/components/');
+const pageNextPath = path.resolve(__dirname, '../../pages/');
+const potFileContent = {
+    "////////// NOTE: Generated with \"node scripts/i18n/generate-translation-file.js\", you could change the language value you want.": "////////// 提示: 此文件由 \"node scripts/i18n/generate-translation-file.js\" 命令自动生成，您可以修改属性值为想要的语言",
+    "English Characters": "中文字符串"
+};
+
+const recursiveSync = (src, dest) => {
+    const exists = fs.existsSync(src);
+    const stats = exists && fs.statSync(src);
+    const isDirectory = exists && stats.isDirectory();
+    if (isDirectory) {
+        fs.readdirSync(src).forEach(function (childItemName) {
+            
+            if ( 
+                childItemName.indexOf('.tsx') !== -1 || 
+                childItemName.indexOf('.js') !== -1 || 
+                childItemName.indexOf('.ts') !== -1) 
+            {
+                
+                const filePath = path.join(src, childItemName);
+
+                // find the translation string from the file
+                const data = fs.readFileSync(filePath, {encoding:'utf8', flag:'r'});
+                const result = data.match(/t\([\s]?"([^']*?)"[\s]?\)|t\([\s]?'([^"]*?)'[\s]?\)/g);  // ['t("Like")', 't( "Body" )']
+                
+                if ( result !== null ) {
+                    const fields = result.map(function(el) { return el.replace(/^t\([\s]?'|'[\s]?\)$|^t\([\s]?"|"[\s]?\)$/g, ""); });
+        
+                    fields.forEach( function(field) {
+                        potFileContent[field] = '';
+                    });
+                }
+
+            }
+            
+            recursiveSync(path.join(src, childItemName), path.join(dest, childItemName));
+        });
+    }
+};
+
+
+recursiveSync(componentPath, '');
+recursiveSync(pageNextPath, '');
+
+//
+const targetFile = 'src/lang/pot/site.lang.auto-generated.json';
+fs.mkdirSync(path.resolve(__dirname, '../../src/lang/pot/'), { recursive: true });
+fs.writeFile(path.join(path.resolve(__dirname, '../../'), targetFile), JSON.stringify(potFileContent, null, 4), 'utf8', function (err) {
+    if (err) return console.log(err);
+    console.log('\x1b[36m%s\x1b[0m', `--> Generated "${targetFile}" successfully`);
+});
