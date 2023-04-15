96e220363cb36822ac2c19a9140661cae3704f99
UIUX Lab <uiuxlab@gmail.com>
2023-04-15 10:10:24 +0800

    Added a tool for inheriting multiple classes

---
+++ src/utils/multiple-inheritance.js
@@ -0,0 +1,33 @@
+/**
+ * Class Multiple inheritance
+ * @param {Constructor} BaseClass      - Parent Classes
+ * @returns Constructor
+ */
+/*
+Usage:
+
+class BaseClass { }
+const GrpcService = multipleClasses(ExtensionOne, ExtensionTwo)(BaseClass);
+
+export default new GrpcService;
+
+*/
+export function multipleClasses(...mixins) {
+    return function (Base) {
+        const copyProps = (target, source) => {
+            Object.getOwnPropertyNames(source)
+                .concat(Object.getOwnPropertySymbols(source))
+                .forEach((prop) => {
+                    if (prop.match(/^(?:constructor|prototype|arguments|caller|name|bind|call|apply|toString|length)$/)) {
+                        return;
+                    }
+                    Object.defineProperty(target, prop, Object.getOwnPropertyDescriptor(source, prop));
+                });
+        };
+        mixins.forEach((mixin) => {
+            copyProps(Base, mixin);
+            copyProps(Base.prototype, mixin.prototype);
+        });
+        return Base;
+    }
+}
