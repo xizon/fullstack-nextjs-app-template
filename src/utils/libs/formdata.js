e2e9a8371670b8656b235fbe6141831e7513a7b6
UIUX Lab <uiuxlab@gmail.com>
2023-06-14 15:47:42 +0800

    update utils

--- src/utils/libs/formdata.js
+++ src/utils/libs/formdata.js
@@ -7,50 +7,47 @@
 function serializeArray(form, types = ['input', 'textarea', 'select', 'checkbox', 'progress', 'datalist']) {
 
     const objects = [];
-    if (typeof form == 'object' && form.nodeName.toLowerCase() == "form") {
+    const fieldsTypes = types;
+    
+    fieldsTypes.map((item, index) => {
+        const fields = form.getElementsByTagName(item);
+        for (let i = 0; i < fields.length; i++) {
 
-        const fieldsTypes = types;
-        fieldsTypes.map((item, index) => {
-            const fields = form.getElementsByTagName(item);
-            for (let i = 0; i < fields.length; i++) {
+            const _name = fields[i].getAttribute("name");
+            let _value = fields[i].value;
 
-                const _name = fields[i].getAttribute("name");
-                let _value = fields[i].value;
-
-                // if field is Array
-                if ( _name !== null && _name.match(/(\[.*?\])/gi) ) {
-                    const inputs = form.querySelectorAll("[name='"+_name+"']");
-                    const _arrFieldValue = [];
-                    for (let j = 0; j < inputs.length; j++) {
-                        const _arrField = inputs[j];
-                        _arrFieldValue.push(_arrField.value);
-                    }
-
-                    _value = _arrFieldValue;
+            // if field is Array
+            if ( _name !== null && _name.match(/(\[.*?\])/gi) ) {
+                const inputs = form.querySelectorAll("[name='"+_name+"']");
+                const _arrFieldValue = [];
+                for (let j = 0; j < inputs.length; j++) {
+                    const _arrField = inputs[j];
+                    _arrFieldValue.push(_arrField.value);
                 }
 
-                
-                //if checkbox or radio
-                if ( fields[i].type === 'radio' || fields[i].type === 'checkbox' ) {
-                    if ( fields[i].checked === true ) {
-                        objects[objects.length] = {
-                            name: _name,
-                            value: _value
-                        };
-                    }
-                } else {
+                _value = _arrFieldValue;
+            }
+
+            
+            //if checkbox or radio
+            if ( fields[i].type === 'radio' || fields[i].type === 'checkbox' ) {
+                if ( fields[i].checked === true ) {
                     objects[objects.length] = {
                         name: _name,
                         value: _value
                     };
                 }
-           
-
+            } else {
+                objects[objects.length] = {
+                    name: _name,
+                    value: _value
+                };
             }
-        });
+        
 
-    }
-    
+        }
+    });
+
     // remove Duplicate objects from JSON Array
     const clean = objects.filter((item, index, self) => index === self.findIndex((t) => (t.name === item.name)));
 
