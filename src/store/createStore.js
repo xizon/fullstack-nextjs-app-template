559d2e907746c7c8079f5d55294b0d1e1c4382f6
UIUX Lab <uiuxlab@gmail.com>
2025-01-02 17:15:57 +0800

    update redux, redux-thunk

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,5 +1,5 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 import { createWrapper } from "next-redux-wrapper";
 import rootReducer from "./reducers";
 
