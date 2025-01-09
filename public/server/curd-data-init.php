7abc50687a4f6ef381e641ebf59141ac5db56cc5
UIUX Lab <uiuxlab@gmail.com>
2025-01-09 15:23:33 +0800

    add global.d.ts and change server files

--- public/server/curd-data-init.php
+++ public/server/curd-data-init.php
@@ -5,8 +5,8 @@ header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Credentials: true');
 header('Content-type: application/json');
 
-$data_file = 'curd-data/posts.backup.json';
+$data_file = __DIR__ . '/curd-data/posts.backup.json';
 $jsondata = file_get_contents($data_file);
-file_put_contents('curd-data/posts.json', $jsondata);
-
+echo $jsondata;
+die();
 
---
+++ src/global.d.ts
@@ -0,0 +1,28 @@
+/**
+ * Create a declaration file for image resources
+ */
+
+declare module '*.png' {
+    const value: string;
+    export default value;
+  }
+  declare module '*.jpg' {
+    const value: string;
+    export default value;
+  }
+  declare module '*.jpeg' {
+    const value: string;
+    export default value;
+  }
+  declare module '*.svg' {
+    const value: string;
+    export default value;
+  }
+  declare module '*.gif' {
+    const value: string;
+    export default value;
+  }
+  declare module '*.webp' {
+    const value: string;
+    export default value;
+  }
