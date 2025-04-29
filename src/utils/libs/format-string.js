64ae37546c81d7185df03771be8aceb7b4e82316
UIUX Lab <uiuxlab@gmail.com>
2025-04-29 16:25:56 +0800

    update format-string utility

--- src/utils/libs/format-string.js
+++ src/utils/libs/format-string.js
@@ -7,52 +7,52 @@
 
 // Remove all special characters except space from a string using JavaScript
 function rmSpec(input) {
-    return input.replace(/[^a-zA-Z0-9 \u4E00-\u9FFF]/g, "");
+    return input?.replace(/[^a-zA-Z0-9 \u4E00-\u9FFF]/g, "");
 }
 
 //only number and letters are allowed
 function onlyNumAndLetter(input) {
-    return input.replace(/[^a-zA-Z0-9 ]/g, "");
+    return input?.replace(/[^a-zA-Z0-9 ]/g, "");
 }
 
 //remove all spaces including the middle
 function rmAllSpace(input) {
-    return input.replace(/\s/g, "");
+    return input?.replace(/\s/g, "");
 }
 
 //remove whitespace from both sides of a string.
 function trimAll(input) {
-    return input.replace(/(^\s+)|(\s+$)/g, "");
+    return input?.replace(/(^\s+)|(\s+$)/g, "");
 }
 
 //replace multiple spaces with a single space
 function multiSpacesToSingle(input) {
-    return input.replace(/\s+(\W)/g, ' ');
+    return input?.replace(/\s+(\W)/g, ' ');
 }
 
 //convert HTML text to plain text
 function htmlToPlain(input) {
-    return input.replace(/(<([^>]+)>)/ig, '');
+    return input?.replace(/(<([^>]+)>)/ig, '');
 }
 
 //strip tags and content
 function stripTagsAndContent(input) {
-    return input.replace(/<\/?[^>]+(>|$)(.*?)<\/?[^>]+(>|$)/ig, '');
+    return input?.replace(/<\/?[^>]+(>|$)(.*?)<\/?[^>]+(>|$)/ig, '');
 }
 
 //remove first and last slash from specific url
 function removeFirstLastSlash(input) {
-    return input.replace(/^\/|\/$/g, '');
+    return input?.replace(/^\/|\/$/g, '');
 }
 
 //remove trailing slash from specific url
 function removeTrailingSlash(input) {
-    return input.replace(/\/+$/, '');
+    return input?.replace(/\/+$/, '');
 }
 
 //remove first slash from specific url
 function removeFirstSlash(input) {
-    return input.replace(/\//, '');
+    return input?.replace(/\//, '');
 }
 
 export {
