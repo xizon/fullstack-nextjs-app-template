f520f3ab46b9d43905e5846629b50e8c237f0e8c
Goutham-puram <puram.goutham@gmail.com>
2025-07-08 11:01:24 -0700

    Update .eslintrc.js
    updated '@next/next/no-html-link-for-pages': 'off'

--- backend@nest/.eslintrc.js
+++ backend@nest/.eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@next/next/no-html-link-for-pages': 'off'
   },
 };
